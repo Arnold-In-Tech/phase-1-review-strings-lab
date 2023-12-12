d31c4b6c8134169696603f2785dbf0a4e078ca16
Arnold-In-Tech <g16a7782@gmail.com>
2023-12-13 02:50:20 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+
+// Use if + operator for string concatenation
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+
+
+// String interpolation
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+// OR
+// const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
